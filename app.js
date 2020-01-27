de2d49f1801a6f17ab1e45e3fada2a920e90f45f
Scott Kumor <scott.kumor1212@gmail.com>
2020-01-26 20:03:37 -0600

    added cards

--- app.js
+++ app.js
@@ -48,78 +48,103 @@ $(document).ready(function () {
     $('#contentWrapper').slideUp('slow', function () {
       $('#contentWrapper').attr('style', "display:block");
       $('#contentWrapper').html(`
-        <div
-        class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-0 p-m s-hv">
-        <!--grid box wrapper-->
-        <a
-          class="ai-c d-f jc-c df-fdc c-w td-n"
-          href="https://github.com/scottkumor"
-        > <!--link to page or project-->
-          <div class="c-db ta-c">Github Page</div>
-          <img
-            class="i-fl mt-l"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png"
-          />
-        </a>
-      </div>
       <div
-        class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-1 p-m s-hv"
-      >
-        <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Password-Generator/">
-          <div class="c-db ta-c">Password Generator</div>
-          <img class="i-fl mt-l" src="./assets/images/soon5.jpg" />
-        </a>
-      </div>
-      <div
-        class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-2 p-m s-hv"
-      >
-        <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Day-Planner/">
-          <div class="c-db ta-c">Day Planner</div>
-          <img class="i-fl mt-l" src="./assets/images/soon3.jpg" />
-        </a>
-      </div>
-      <div
-        class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-3 p-m s-hv"
-      >
-        <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Weather-Dashboard/">
-          <div class="c-db ta-c">Weather Dashboard</div>
-          <img class="i-fl mt-l" src="./assets/images/soon4.jpg" />
-        </a>
-      </div>
-      
-      </div>
-      <div
-        class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-4 p-m s-hv"
-      >
-        <a class="ai-c d-f jc-c df-fdc c-w td-n" href="">
-          <div class="c-db ta-c">Profile Generator</div>
-          <img class="i-fl mt-l" src="./assets/images/soon2.jpg" />
-        </a>
-      
-      <div
-        class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-5 p-m s-hv">
-        <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Dev-Profile-Generator/">
-          <div class="c-db ta-c">Note Taker</div>
-          <img class="i-fl mt-l" src="./assets/images/soon.jpg" />
-        </a>
-      </div>  
-      <div
-        class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-5 p-m s-hv">
-        <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Dev-Profile-Generator/">
-          <div class="c-db ta-c">Note Taker</div>
-          <img class="i-fl mt-l" src="./assets/images/soon.jpg" />
-        </a>
-      </div>  
+      class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-0 p-m s-hv">
+      <!--grid box wrapper-->
+      <a
+        class="ai-c d-f jc-c df-fdc c-w td-n"
+        href="https://github.com/scottkumor"
+      > <!--link to page or project-->
+        <div class="c-db ta-c">Github Page</div>
+        <img
+          class="i-fl mt-l"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/600px-Octicons-mark-github.svg.png"
+        />
+      </a>
+    </div>
+    <div
+      class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-1 p-m s-hv"
+    >
+      <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Password-Generator/">
+        <div class="c-db ta-c">Password Generator</div>
+        <img class="i-fl mt-l" src="./assets/images/soon5.jpg" />
+      </a>
+    </div>
+    <div
+      class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-2 p-m s-hv"
+    >
+      <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Day-Planner/">
+        <div class="c-db ta-c">Day Planner</div>
+        <img class="i-fl mt-l" src="./assets/images/soon3.jpg" />
+      </a>
+    </div>
+    <div
+      class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-3 p-m s-hv"
+    >
+      <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Weather-Dashboard/">
+        <div class="c-db ta-c">Weather Dashboard</div>
+        <img class="i-fl mt-l" src="./assets/images/soon4.jpg" />
+      </a>
+    </div>
+    
+    </div>
+    <div
+      class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-4 p-m s-hv"
+    >
+      <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/Dev-Profile-Generator/">
+        <div class="c-db ta-c">Profile Generator</div>
+        <img class="i-fl mt-l" src="./assets/images/soon2.jpg" />
+      </a>
+    
+    <div
+      class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-5 p-m s-hv">
+      <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://scottkumor.github.io/note-taker/">
+        <div class="c-db ta-c">Note Taker</div>
+        <img class="i-fl mt-l" src="./assets/images/soon.jpg" />
+      </a>
+    </div>  
+    <div
+      class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-5 p-m s-hv">
+      <a class="ai-c d-f jc-c df-fdc c-w td-n" href="">
+        <div class="c-db ta-c"></div>
+        <img class="i-fl mt-l" src="./assets/images/soon.jpg" />
+      </a>
+    </div>
+  <div
+    class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-5 p-m s-hv">
+    <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://da-burger-smk.herokuapp.com/">
+      <div class="c-db ta-c">Database CLI with "Burgers"</div>
+      <img class="i-fl mt-l" src="./assets/images/soon.jpg"/>
+    </a>
+  </div>
+  <div
+    class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-5 p-m s-hv">
+    <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://hot-restaurant-scottkumor.herokuapp.com/">
+      <div class="c-db ta-c">Restaurant Reservations</div>
+      <img class="i-fl mt-l" src="./assets/images/soon.jpg"/>
+    </a>
+  </div>
+  <div
+    class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-5 p-m s-hv">
+    <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://star-wars-scottkumor.herokuapp.com/">
+      <div class="c-db ta-c">Create Your Own Star Wars Character</div>
+      <img class="i-fl mt-l" src="./assets/images/soon.jpg"/>
+    </a>
+  </div>
+  
+    <div class="m-s ta-c ts-i">Collaborations</div>
         
-        
-        
-        
-        <div class="m-s ta-c ts-i">More Projects coming soon.</div>`
+    <div
+      class="bg-c-lb-hv bg-c-db-fc c-w-hv c-w-fc t-g-hv t-s-fc gbx-p-5 p-m s-hv">
+      <a class="ai-c d-f jc-c df-fdc c-w td-n" href="https://iedson.github.io/spotlight/">
+        <div class="c-db ta-c">Spotlight</div>
+        <img class="i-fl mt-l" src="./assets/images/soon.jpg" />
+      </a>
+    </div>    
+    
+        `
       )
     });
-
-    // for the later cards
-    /*  */
   });
 
   $("#contactBtn").on("click", function () {
